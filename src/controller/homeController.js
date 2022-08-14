39bb24d6eb3f0eb854f5aa8de9ea3455c82f0cb7
lamdangtung <lam.dt26042000@gmail.com>
2022-08-14 15:50:03 +0700

    add edit && delete

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -18,13 +18,36 @@ let getDetailUser = async (req, res) => {
 let insertNewUser = async (req, res) => {
     let body = req.body;
     let {firstName,lastName,email,address} = req.body;
-    await pool.execute("    INSERT INTO users (firstName, lastName, email, address) VALUES (?, ?, ?, ?)", [firstName, lastName, email, address]);
+    await pool.execute("INSERT INTO users (firstName, lastName, email, address) VALUES (?, ?, ?, ?)", [firstName, lastName, email, address]);
     const [rows, fields] = await pool.execute('SELECT * FROM `users`');
+    return res.redirect("/user");
+}
+
+let deleteUser = async (req, res) => {
+    let userId = req.params["userId"];
+    await pool.execute("DELETE FROM users WHERE users.id = ?", [userId]);
+    return res.redirect("/user");
+}
+
+let goEditUser = async (req, res) => {
+    let userId = req.params["userId"];
+    const [rows,fields] = await pool.execute("select * from users where users.id = ?", [userId]);
+    return res.render("edit.ejs", {"user": rows[0]});
+}
+
+let editUser = async (req, res) => {
+    let body = req.body;
+    let userId = req.params["userId"];
+    let {firstName,lastName,email,address} = req.body;
+    await pool.execute("UPDATE users SET users.firstName = ?, users.lastName = ?, users.email = ?, users.address = ? WHERE users.id = ?" ,[firstName,lastName,email,address,userId]);
     return res.redirect("/user");
 }
 
 module.exports = {
     getAllUsers,
     getDetailUser,
-    insertNewUser
+    insertNewUser,
+    deleteUser,
+    goEditUser,
+    editUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,9 @@ const initWebRoute = (app) => {
     router.get('/user', homeController.getAllUsers)
     router.get('/user/:userId', homeController.getDetailUser)
     router.post('/user', homeController.insertNewUser)
+    router.post('/user/:userId', homeController.deleteUser)
+    router.get('/user/edit/:userId', homeController.goEditUser)
+    router.post('/user/edit/:userId', homeController.editUser)
     return app.use('/', router);
 }
 
